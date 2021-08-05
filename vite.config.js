abccb000bda3e20712aa982efd5c057241ab367e
Jake Klassen <jklassendev@gmail.com>
2021-08-05 13:06:03 -0400

    remove unused config

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,6 @@
  * @type {import('vite').UserConfig}
  */
 const config = {
-  assetsInclude: [
-    "./src/assets/maps/desert-external-tilesets/desert.json",
-    "./src/assets/maps/desert-external-tilesets/water.json",
-  ],
   // We can't have base64 images - they won't work with Phaser tilemaps
   assetsInlineLimit: 0,
 };
